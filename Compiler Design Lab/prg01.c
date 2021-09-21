71d4c08a04d418a136f6ad143998f521749b5084
Sumit Kumar <vishnusumit2@gmail.com>
2021-09-21 21:25:56 +0530

    Compiler design lab directory created.... program 1 created

---
+++ Compiler Design Lab/prg01.c
@@ -0,0 +1,28 @@
+#include"stdio.h"
+#include"string.h"
+int main()
+{
+    char s[50];
+    int flag=1,le, i;
+    printf("Enter String: ");
+    scanf("%s",&s);
+    le = strlen(s);
+    if(s[0]!='0' || s[1]!='1' || s[2]!='1'){
+        flag=0;
+    }
+    if(flag==1){
+        for(i=3; i<le; i++){
+            if(s[i]!='0' && s[i]!='1'){
+                flag=0;
+                break;
+            }
+        }
+    }
+    if(flag==1){
+        printf("String Accepted!!");
+    }
+    else{
+        printf("String Not Accepted!!");
+    }
+    return 0;
+}
