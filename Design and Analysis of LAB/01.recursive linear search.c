500493524ad5ef6e70af8f3ab861ddfec4339203
Sumit Kumar <71210420+ivsumitkumar@users.noreply.github.com>
2021-09-25 00:12:44 +0530

    DAA LAB
    programs 1 and 2 done!!

---
+++ Design and Analysis of LAB/01.recursive linear search.c
@@ -0,0 +1,24 @@
+//WAP in c to implement recursive linear search.
+#include"stdio.h"
+#include"conio.h"
+int linearsearch(int key, int i){
+	int a[10]={0,1,2,3,4,5,6,7,8,9};
+	if (i==10){
+		printf("\nElement not present.");
+		return 0;
+	}
+	else if(a[i]==key){
+		printf("\nElement Found at index %d",i);
+		return 0;
+	}
+	else
+		return linearsearch(key,i+1);
+}
+void main()
+{
+	int key;
+	printf("Enter element to search: ");
+	scanf("%d",&key);
+	linearsearch(key,0);
+	getch();
+}
---
+++ Design and Analysis of LAB/02.recursive binary search.c
@@ -0,0 +1,26 @@
+//WAP in C to implement recursive binary search
+#include"stdio.h"
+#include"conio.h"
+int binarysearch(int key, int min,int max){
+	int a[10]={0,1,2,3,4,5,6,7,8,9};
+	int mid=((min+max)/2);
+	if(min>mid || key>max){
+		printf("\nElement not present.");
+		return 0;
+	}
+	else if(a[mid]==key){
+		printf("\nElement Found at index %d",mid);
+		return 0;
+	}
+	else if(a[mid]<key)
+		return binarysearch(key,mid+1,max);
+	else if(a[mid]>key)
+		return binarysearch(key,min,mid-1);
+}
+int main(){
+	int key;
+	printf("Enter the key: ");
+	scanf("%d",&key);
+	binarysearch(key,0,9);
+	return 0;
+}
