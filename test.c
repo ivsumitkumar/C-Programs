32d9e6e9cc3fa27f221b46b8bc6f017baef99bc4
Sumit Kumar <71210420+ivsumitkumar@users.noreply.github.com>
2021-12-31 22:29:38 +0530

    kljj

--- test.c
+++ test.c
@@ -1,18 +1,11 @@
-#include <stdio.h>
-int main() {
-    int n1, n2, max;
-    printf("Enter two positive integers: ");
-    scanf("%d %d", &n1, &n2);
-
-    // maximum number between n1 and n2 is stored in max
-    max = (n1 > n2) ? n1 : n2;
-
-    while (1) {
-        if (max % n1 == 0 && max % n2 == 0) {
-            printf("The LCM of %d and %d is %d.", n1, n2, max);
-            break;
-        }
-        ++max;
-    }
-    return 0;
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c,d,result;
+printf("enter first three number\n");
+scanf("%d%d%d%d",&a,&b,&c,&d);
+result=((a>b && a>c && a>d) ? a : (b>c && b>d) ? b : (c>d)? c : d );
+printf("%d is greater",result);
+getch();
 }
